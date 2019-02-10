436f49dfd781069f9394009952278ae0027b9885
Lizzie Coady <lizziecoady@hotmail.co.uk>
2019-02-10 18:41:57 +0000

    Added weather and applied to land and take_off

--- lib/airport.rb
+++ lib/airport.rb
@@ -6,16 +6,31 @@ class Airport
 
   def initialize
     @airport = []
-    # change 'Plane 1' & 'Plane 2' to instances of the Plane class
+  end
+
+  def display_planes
+    @airport
   end
 
   def land(plane)
+    fail 'Weather is stormy, cannot land plane' unless weather == 'Sunny'
     @airport.push(plane)
   end
 
   def take_off(plane)
+    fail 'Weather is stormy, cannot take off' unless weather == 'Sunny'
     @airport.delete(plane)
     plane
+  end
+
+  def weather
+    random_num = rand(5)
+    if (random_num == 0 || random_num == 1 || random_num == 2 || random_num == 3)
+      @condition = 'Sunny'
+    elsif random_num == 4
+      @condition = 'Stormy'
+    end
+    @condition
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,14 +2,54 @@ require 'airport'
 
 describe Airport do
 
-  it 'lands a plane' do
-    plane = Plane.new('name')
-    expect(subject.land(plane)).to eq [plane]
+  it 'displays the planes in the airport' do
+    expect(subject.display_planes).to eq []
   end
 
-  it 'takes off a plane' do
-    plane = Plane.new('name')
-    expect(subject.take_off(plane)).to eq plane
+
+  describe '#land' do
+
+    it 'raises an error when trying to land in stormy weather' do
+      plane = Plane.new('name')
+      expect{subject.land(plane)}.to raise_error 'Weather is stormy, cannot land plane'
+    end
+
+    it 'lands a plane' do
+      plane = Plane.new('name')
+      expect(subject.land(plane)).to eq [plane]
+    end
+
+  end
+
+
+  describe '#take_off' do
+
+    it 'raises an error when trying to take off in stormy weather' do
+      plane = Plane.new('name')
+      expect{subject.take_off(plane)}.to raise_error 'Weather is stormy, cannot take off'
+    end
+
+    it 'takes off a plane' do
+      plane = Plane.new('name')
+      expect(subject.take_off(plane)).to eq plane
+    end
+
+  end
+
+
+  describe '#weather' do
+
+    it 'checks if the weather is sunny' do
+      # below line is a stub
+      allow(subject).to receive(:rand).and_return(0)
+      expect(subject.weather).to eq('Sunny')
+    end
+
+    it 'checks if the weather is stormy' do
+      allow(subject).to receive(:rand).and_return(4)
+      expect(subject.weather).to eq('Stormy')
+    end
+
   end
 
 end
